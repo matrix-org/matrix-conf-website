fbabfc1923e4f1315b366166781c379b2e441d22
Thib <thib@ergaster.org>
2025-07-07 15:16:07 +0200

    Change the public and build directories to be consistent with Zola
    Signed-off-by: Thib <thib@ergaster.org>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    publicDir: "./static",  // Updating this value and the next for consistency
+    outDir: "./public",     // with how Zola was deployed, to keep CF Pages happy.
     site: "https://conference.matrix.org",
     integrations: [sitemap()],
     image: {
