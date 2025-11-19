6758b5e7ac938495f818289c1517e4232b20a9a0
Thib <thib@ergaster.org>
2025-11-19 18:20:06 +0100

    Split the session data into a Content Collection
    Signed-off-by: Thib <thib@ergaster.org>

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const talks = defineCollection({
+    loader: glob({pattern: "**/session.json", base: "./src/talks"}),
+    schema: z.object({
+        ID: z.string(),
+        "Proposal title": z.string(),
+        Track: z.object({
+            en: z.string()
+        }).optional(),
+        "Speaker names": z.array(z.string()),
+        Room: z.object({
+            en: z.string()
+        }),
+        Start: z.string(),
+        youtube: z.string().url().optional()
+    })
+});
+
+export const collections = { "talks": talks };
