22232c93aa0b1db0f3369ded4ad67354b2cb8672
MTRNord <MTRNord@users.noreply.github.com>
2026-09-25 19:59:04 +0200

    Add the pretalx schedule data loader
    Signed-off-by: MTRNord <MTRNord@users.noreply.github.com>

---
+++ src/data/pretalx.ts
@@ -0,0 +1,6 @@
+// Where the 2026 pretalx (CfP/schedule) instance lives. Shared by everything that renders the schedule.
+const event = "https://cfp.2026.matrix.org/matrix-conference-2026/";
+
+export const pretalxSchedulePage = `${event}schedule/`;
+export const pretalxSchedule = `${event}schedule/widget/v2.json`;
+export const pretalxTalkBase = `${event}talk/`;
